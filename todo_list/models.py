35e7fd31297161eaf2ee901aee2826bfbdd9c783
Chaithanya S Poojary <chaitanyaspoojary17@gmail.com>
2026-02-18 21:27:18 +0530

    Delete todo_list directory

--- todo_list/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-    title = models.CharField(max_length=100, null = False, blank = False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def _str_(self):
-        return self.title
--- todo_list/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Todo
-
-# Create your views here.
-def home(request):
-    tasks = Todo.objects.all()
-    return render(request,'home.html',{'tasks': tasks})
